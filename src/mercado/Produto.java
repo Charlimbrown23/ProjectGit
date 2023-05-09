3877f8e02fb6a4c514446e041bb64f1b1d1d5309
Charlimbrown23 <gustavo.ha04@aluno.ifsc.edu.br>
2023-05-09 17:14:49 -0300

    ola

---
+++ src/mercado/Produto.java
@@ -0,0 +1,63 @@
+package mercado;
+
+public class Produto {
+
+	private long codigo;
+	private String descricao;
+	private int quantidade;
+	private double valor;
+	
+	public Produto(long codigo,String descricao, int quantidade,double valor) {
+		this.codigo=codigo;
+		this.descricao=descricao;
+		this.quantidade=quantidade;
+		this.valor=valor;
+	}
+	
+	public long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	public double getValor() {
+		return valor;
+	}
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	
+	public String consultar(int codigo) {
+		String consulta="";
+		
+		consulta += "\n ------------------------";
+		consulta += "\n Código:"+this.codigo;
+		consulta += "\n Descrição:"+this.descricao;
+		consulta += "\nQuantidade:"+this.quantidade;
+		consulta += "\n valor:"+this.valor;
+		consulta += "\n ------------------------";
+		
+		
+		return consulta;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/mercado/TesteMercado.java
@@ -0,0 +1,26 @@
+package mercado;
+
+public class TesteMercado {
+
+	
+	public static void main(String[] args) {
+
+		Produto arroz = new Produto(1, "Comida", 10, 15.50);
+		
+		System.out.println("Produto arroz:");
+		System.out.println(arroz.getCodigo());
+		System.out.println(arroz.getDescricao());
+		System.out.println(arroz.getQuantidade());
+		System.out.println(arroz.getValor());
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
